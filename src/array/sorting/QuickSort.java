5368d295f47cc7b8489cf92094298599b14167d6
Pratap Singh <pratapsingh839399@gmail.com>
2026-02-09 00:13:19 +0800

    QuickSort

---
+++ src/array/sorting/QuickSort.java
@@ -0,0 +1,38 @@
+package array.sorting;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] array = {5, 62, 2, 111, 81, 4,3,-1};
+        quickSort(array, 0, array.length - 1);
+
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+}
